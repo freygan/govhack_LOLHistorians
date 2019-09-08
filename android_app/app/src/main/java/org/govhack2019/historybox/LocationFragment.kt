12829de5a8b828dd8642931b7d9fc4151f6d5162
Peter Scully <peter@peterscully.name>
2019-09-08 11:34:36 +1000

    Add layout for Location fragment

--- android_app/app/src/main/java/org/govhack2019/historybox/LocationFragment.kt
+++ android_app/app/src/main/java/org/govhack2019/historybox/LocationFragment.kt
@@ -10,12 +10,12 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class DetailFragment : Fragment() {
+class LocationFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        return inflater.inflate(R.layout.fragment_location, container, false)
     }
 
 
